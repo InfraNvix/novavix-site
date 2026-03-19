ef57f29c9bdd0f717ecefc2b5a3ba2d9d7f0427b
InfraNvix <infraestrutura_empresa@novavix.com.br>
2026-03-19 10:49:58 -0300

    Update Studio.tsx

--- app/admin/[[...index]]/Studio.tsx
+++ app/admin/[[...index]]/Studio.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
 
-export default function Studio() {
+export default function Studio({ config }: { config: any }) {
   return <NextStudio config={config} />
 }
